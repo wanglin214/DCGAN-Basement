dd4fb23a16f4bcb30fdde6ca28a4fbcfd5ac3b34
AiLin <103014845+wanglin214@users.noreply.github.com>
2025-04-08 09:13:41 +0800

    Update DCGAN.py

--- DCGAN/model/DCGAN.py
+++ DCGAN/model/DCGAN.py
@@ -1,19 +1,12 @@
-# coding:utf8
-
-# Copyright 2019 longpeng2008. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# If you find any problem,please contact us
-#
-#     longpeng2008to2012@gmail.com
-#
-# or create issues
+# =============================================================================
+# version 2025-04-08.
 # =============================================================================
 import torch
 import torch.nn as nn
 import math
 
 
-# # 定义生成器与判别器
+# # Define the generator and discriminator
 class Generator(nn.Module):
     def __init__(self, nz=200, ngf=64, nc=1):
         super(Generator, self).__init__()
@@ -21,23 +14,23 @@ class Generator(nn.Module):
         self.nz = nz
         self.nc = nc
         self.main = nn.Sequential(
-            # 输入噪声向量Z，输出(ngf*8) x 4 x 4特征图
+            # Input noise vector Z, output (ngf*8) x 4 x 4 feature map
             nn.ConvTranspose2d(nz, ngf * 16, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 16),
             nn.ReLU(True),
-            # 输入(ngf*8) x 4 x 4特征图，输出(ngf*4) x 8 x 8
+            # Input (ngf*8) x 4 x 4 feature map, output (ngf*4) x 8 x 8
             nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # 输入(ngf*4) x 8 x 8，输出(ngf*2) x 16 x 16
+            # Input (ngf*4) x 8 x 8, output (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # 输入(ngf*2) x 16 x 16，输出(ngf) x 32 x 32
+            # Input (ngf*2) x 16 x 16, output (ngf) x 32 x 32
             nn.ConvTranspose2d(ngf * 4, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # 输入(ngf) x 32 x 32，输出(nc) x 64 x 64
+            # Input (ngf) x 32 x 32, output (nc) x 64 x 64
             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
         )
@@ -52,26 +45,26 @@ class Discriminator(nn.Module):
         self.ndf = ndf
         self.nc = nc
         self.main = nn.Sequential(
-            # 输入图片大小 (nc) x 64 x 64，输出 (ndf) x 32 x 32
+            # Input image size (nc) x 64 x 64, output (ndf) x 32 x 32
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # 输入(ndf) x 32 x 32，输出(ndf*2) x 16 x 16
+            # Input (ndf) x 32 x 32, output (ndf*2) x 16 x 16
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # 输入(ndf*2) x 16 x 16，输出 (ndf*4) x 8 x 8
+            # Input (ndf*2) x 16 x 16, output (ndf*4) x 8 x 8
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # 输入(ndf*4) x 8 x 8，输出(ndf*8) x 4 x 4
+            # Input (ndf*4) x 8 x 8, output (ndf*8) x 4 x 4
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
 
-            # 输入(ndf*8) x 4 x 4，输出1 x 1 x 1
+            # Input (ndf*8) x 4 x 4, output 1 x 1 x 1
             nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
@@ -80,7 +73,7 @@ class Discriminator(nn.Module):
         return self.main(input)
 
 
-# 遍历生成器和判别器的参数，并应用随机初始化
+# Traverse the parameters of the generator and discriminator and apply random initialization
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
