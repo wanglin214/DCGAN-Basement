76fea9a2ed0aebff8221b355cfe1091ca4f4e8d5
AiLin <103014845+wanglin214@users.noreply.github.com>
2025-04-08 09:30:42 +0800

    Update readGrd.py

--- DCGAN/utils/readGrd.py
+++ DCGAN/utils/readGrd.py
@@ -1,29 +1,42 @@
-##读取异常grd文件与剩余密度模型文件
+## read grd file
 # @Time: 2023/2/17 10:59
 # @Author: WangLin
-# @File: ReadGrd.py:读取grd文件
+# @File: ReadGrd.py: Reading grd files
 # @Software: PyCharm
 
 import os
 import numpy as np
 
 
-# 创建读取grd文件的函数,返回网格文件数组grdata(Ny,Nx),Ny是列数——对应dim=0,
+# Function to read grd files, returns grid file array grdata(Ny,Nx), Ny is the number of columns - corresponding to dim=0
 def readGrdbynp(filepath):
+    """
+    Read Surfer 6 Text format GRD file and return the grid data as numpy array
+    
+    Parameters:
+    -----------
+    filepath : str
+        Path to the GRD file
+        
+    Returns:
+    --------
+    gd : numpy.ndarray
+        Grid data with shape (Ny, Nx)
+    """
     with open(filepath, "r", encoding="UTF-8") as infile:
-        infile.readline()  # 跳过标准surfer 6 text格式文件首行的标注 DSAA
-        str = infile.readline().split()  # 读取第二行点、线数
+        infile.readline()  # Skip the first line of the standard surfer 6 text format file with the label DSAA
+        str = infile.readline().split()  # Read the second line with point and line numbers
         Nx = int(str[0])
         Ny = int(str[1])
         # print(Nx, Ny)
-        str = infile.readline().split()  # 读取第三行X方向最小值、最大值
+        str = infile.readline().split()  # Read the third line with X direction minimum and maximum values
         Xmin = float(str[0])
         Xmax = float(str[1])
         # print(Xmin, Xmax)
-        str = infile.readline().split()  # 读取第四行Y方向最小值、最大值
+        str = infile.readline().split()  # Read the fourth line with Y direction minimum and maximum values
         Ymin = float(str[0])
         Ymax = float(str[1])
-        str = infile.readline().split()  # 读取第五行网格数据最小值、最大值
+        str = infile.readline().split()  # Read the fifth line with grid data minimum and maximum values
         gdmin = float(str[0])
         gdmax = float(str[1])
         # print(gdmin, gdmax)
@@ -34,7 +47,7 @@ def readGrdbynp(filepath):
     return gd
 
 
-# 局部子程序测试
+# Local subroutine test
 if __name__ == '__main__':
     Fgafile = r"D:\Project\DL_interface_inversion\origin_data\dg\FwGravofBasin_0001_Dens_01.grd"
     grddata = readGrdbynp(Fgafile)
